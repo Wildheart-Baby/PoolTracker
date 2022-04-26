b2560b90b20575335b44902a5b68d24f59129034
Simon Bostock <simon.bostock@gl-education.com>
2022-04-26 08:45:22 +0100

    Add new methods
    Add methods to create a played match object from the new match object, and to push the new object to the played match object.

--- src/app/Services/match.service.ts
+++ src/app/Services/match.service.ts
@@ -4,6 +4,8 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Match } from '../Model/match';
 import MatchesJson from '../../assets/matchesData.json';
 import { PlayedMatch } from '../Model/playedMatch';
+import { PlayerService } from './player.service';
+import { MatchEnding } from '../Model/matchEnding';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,13 @@ import { PlayedMatch } from '../Model/playedMatch';
 export class MatchService {
 
   private _matches: BehaviorSubject<PlayedMatch[]>;
+  playedMatch!: PlayedMatch;
 
   private dataStore: {
     matches: PlayedMatch[]
   }  
   
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private playerService: PlayerService) {
     this.dataStore = { matches: [] };
     this._matches = new BehaviorSubject<PlayedMatch[]>([]);
    }
@@ -39,4 +42,27 @@ export class MatchService {
       this._matches.next(Object.assign({}, this.dataStore).matches);
     });     
     }
+
+    /** A method to add a match to the played matches array */
+    addMatch(match: Match): Promise<Match>{
+      return new Promise((resolver, reject) => {
+        match.id = this.dataStore.matches.length + 1;
+        this.dataStore.matches.push(this.newPlayedMatch(match));
+        this._matches.next(Object.assign({}, this.dataStore).matches);
+        resolver(match);
+      })
+    }
+
+    /** A method to create a played match object from the new match object */
+    newPlayedMatch(match: Match): PlayedMatch{
+      this.playedMatch = new PlayedMatch;
+      this.playedMatch.id = this.dataStore.matches.length + 1;
+      this.playedMatch.player1 = this.playerService.getPlayerName(match.player1_id);
+      this.playedMatch.player2 = this.playerService.getPlayerName(match.player1_id);
+      this.playedMatch.winner = this.playerService.getPlayerName(match.winner_id);
+      this.playedMatch.balls_left = match.balls_left;
+      //this.playedMatch.matchEnding = MatchEnding. [match.matchEnding as keyof typeof AnimalEnum];
+      return this.playedMatch;
+    }
+ 
 }
