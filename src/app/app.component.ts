d1fc4a69c96a1d5b77619a4a935b7faeaeb9126f
Simon Bostock <simon.bostock@gl-education.com>
2022-04-28 15:48:26 +0100

    Re-route user when adding new match or new user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { MatchesComponent } from './matches/matches.component';
 import { AddMatchComponent } from './add-match/add-match.component';
 import { DialogService } from 'primeng/dynamicdialog';
 import { AddUserComponent } from './add-user/add-user.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ export class AppComponent implements OnInit{
   items: MenuItem[] =[];
   
 
-constructor(public dialogService: DialogService){}
+constructor(public dialogService: DialogService, private router: Router){}
 
   ngOnInit(): void {
     this.items = [
@@ -39,6 +40,7 @@ constructor(public dialogService: DialogService){}
   }
 
   newPlayer(){
+    this.router.navigate(['/rankings']);
     this.dialogService.open(AddUserComponent, {         
       header: 'New user',     
       width: '40%'
@@ -46,6 +48,7 @@ constructor(public dialogService: DialogService){}
   }
 
   newMatch(){
+    this.router.navigate(['/matches']);
     this.dialogService.open(AddMatchComponent, {         
       header: 'New match',     
       width: '40%'
